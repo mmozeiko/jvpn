e2aaf82e276943f4f77bb19b084e041860b6ddda
Martins Mozeiko <martins.mozeiko@gmail.com>
2013-03-10 17:47:56 -0700

    Updated setup script. Removed unneded future import from jvpn.py

--- jvpn_setup.sh
+++ jvpn_setup.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+set -e
 
-cd ~/.juniper_networks/network_connect
+NC=~/.juniper_networks/network_connect
 
-curl -O https://$HOST/dana-cached/nc/ncLinuxApp.jar
-unzip ncLinuxApp.jar ncsvc libncui.so
+if [ -d "$NC" ];
+then
+  echo "$NC already exists, please remove it"
+  exit 1
+fi
+
+mkdir -p $NC
+
+unzip -o -q ncLinuxApp.jar ncsvc libncui.so
+
+mv ncsvc $NC/
+mv libncui.so $NC/
+cp wrapper.c $NC/
+
+cd $NC
+
 gcc -m32 -O2 -s wrapper.c -o ncui -ldl -Wl,-rpath,`pwd`
+chmod +x libncui.so
 sudo chown root:root ncui
 sudo chmod 04755 ncui
+sudo chown root:root ncsvc
+sudo chmod 04755 ncsvc
